cf1ea617b0c705b4db0f0d848356623e29acbff2
khushbu <khushbusoni475@gmail.com>
2023-07-08 00:39:01 +0530

    [khushbu] modify refactor employee wage by function

--- EmpWage.js
+++ EmpWage.js
@@ -1,26 +1,29 @@
 console.log("Welcome to the Employee Wage program")
 
-////UC2 Employee Wage Based on part time and full time////
+////UC3 Refactor Employee Wage by Function ////
 let IS_FULL_TIME = 1;
 let IS_PART_TIME = 2;
 let EMP_RATE_PER_HOUR=20;
 let empHrs=0;
 let empWage=0;
 empCheck= (Math.floor(Math.random()*10)%3)
+function GetWorkingHrs()
+{  
 switch(empCheck)
 {
     case IS_FULL_TIME:
         console.log("Employee is full time present");
         empHrs=8;
-        break;
+        return empHrs;
     case IS_PART_TIME:
         console.log("Employee is part time present");
         empHrs=4;
-        break;
+        return empHrs;
     default:
         console.log("Employee is absent");
-        break;
+        return empHrs;
+}
 }
 
-empWage= (empHrs*EMP_RATE_PER_HOUR)
+empWage= GetWorkingHrs()*EMP_RATE_PER_HOUR
 console.log("Employee Daily Wage:"+ empWage);
