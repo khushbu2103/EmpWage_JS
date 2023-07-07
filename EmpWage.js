aa6b9c8108017b2167b3424f980635c9d59f9de4
khushbu <khushbusoni475@gmail.com>
2023-07-07 23:53:36 +0530

    [khushbu] modify Checking Employee Attendence

--- EmpWage.js
+++ EmpWage.js
@@ -1,1 +1,14 @@
 console.log("Welcome to the Employee Wage program")
+
+////UC1 Checking Employee Attendence////
+let IS_FULLTIME = 1
+empCheck= (Math.floor(Math.random()*10)%2)
+
+if(empCheck==IS_FULLTIME)
+{
+    console.log("Employee is present")
+}
+else
+{
+    console.log("Employee is absent")
+}
