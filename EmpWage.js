dab77d124f1b6edf60530d7a9c2dcb678429e8fd
khushbu <khushbusoni475@gmail.com>
2023-07-08 00:22:21 +0530

    [khushbu] modify Employee wage based on part time and full time

--- EmpWage.js
+++ EmpWage.js
@@ -1,14 +1,26 @@
 console.log("Welcome to the Employee Wage program")
 
-////UC1 Checking Employee Attendence////
-let IS_FULLTIME = 1
-empCheck= (Math.floor(Math.random()*10)%2)
+////UC2 Employee Wage Based on part time and full time////
+let IS_FULL_TIME = 1;
+let IS_PART_TIME = 2;
+let EMP_RATE_PER_HOUR=20;
+let empHrs=0;
+let empWage=0;
+empCheck= (Math.floor(Math.random()*10)%3)
+switch(empCheck)
+{
+    case IS_FULL_TIME:
+        console.log("Employee is full time present");
+        empHrs=8;
+        break;
+    case IS_PART_TIME:
+        console.log("Employee is part time present");
+        empHrs=4;
+        break;
+    default:
+        console.log("Employee is absent");
+        break;
+}
 
-if(empCheck==IS_FULLTIME)
-{
-    console.log("Employee is present")
-}
-else
-{
-    console.log("Employee is absent")
-}
+empWage= (empHrs*EMP_RATE_PER_HOUR)
+console.log("Employee Daily Wage:"+ empWage);
