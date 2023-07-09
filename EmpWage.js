dacb0a4c134d3114e104276f815693aa18e54274
khushbu <khushbusoni475@gmail.com>
2023-07-09 16:36:32 +0530

    [khushbu] modify Calculate employee wage for a month by while loop till the condition

--- EmpWage.js
+++ EmpWage.js
@@ -3,35 +3,40 @@ console.log("Welcome to the Employee Wage program")
 ////UC4 Employee Wage for a month ////
 let IS_FULL_TIME = 1;
 let IS_PART_TIME = 2;
-let EMP_RATE_PER_HOUR=20;
+const EMP_RATE_PER_HOUR=20;
 const NUM_OF_WORKING_DAYS= 20;
-let empHrs=0;
-//let empWage=0;
-let totalWage=0;
+const TOTAL_HOURS_IN_MONTH=160;
 
-function GetWorkingHrs()
+let empHrs=0;
+let empWage=0;
+let totalWage=0;
+let totalEmpHrs=0;
+let day=1;
+function GetWorkingHrs(empCheck)
 {  
 switch(empCheck)
 {
     case IS_FULL_TIME:
-        console.log("Employee is full time present");
+       // console.log("Employee is full time present");
         empHrs=8;
         return empHrs;
     case IS_PART_TIME:
-        console.log("Employee is part time present");
+        //console.log("Employee is part time present");
         empHrs=4;
         return empHrs;
     default:
-        console.log("Employee is absent");
+       // console.log("Employee is absent");
         return empHrs;
 }
 }
-for(let i=0; i<NUM_OF_WORKING_DAYS; i++)
+while(totalEmpHrs<=TOTAL_HOURS_IN_MONTH && day<=NUM_OF_WORKING_DAYS)
 {
-    empCheck= (Math.floor(Math.random()*10)%3)
-    empHrs+= GetWorkingHrs(empCheck)
-   
-    console.log("Employee hrs:"+ empHrs);
+    let empCheck= (Math.floor(Math.random()*10)%3);
+    empWage = GetWorkingHrs(empCheck)*EMP_RATE_PER_HOUR
+    totalWage+=empWage;
+    day++
+    totalEmpHrs+=empHrs;
+    console.log("Employee daily wage for day "+ day + " is "+ empWage)
 }
- let empWage = empHrs * EMP_RATE_PER_HOUR;
-console.log("Employee Total Wage:"+ empWage);
+
+console.log("Employee Total Wage:"+ totalWage);
