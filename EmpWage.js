d0828f4bdabdf83b6efc2faf8b5784f49a93bd81
khushbu <khushbusoni475@gmail.com>
2023-07-11 16:12:18 +0530

    [khushbu] modify calculating wages using array helping functions

--- EmpWage.js
+++ EmpWage.js
@@ -50,3 +50,53 @@ for(let element of EmpDailyWageArray)
     total+=element;
 }
 console.log("Total wage using array: "+ total)
+
+total = 0;
+function GetTotalWage(dailyWage)
+{
+    total+=dailyWage;
+}
+EmpDailyWageArray.forEach(GetTotalWage);
+console.log("UC-7A Using forEach helping function to calculate totalwage: "+total);
+
+function GetTotalWageUsingReduce(totalWage, dailyWage)
+{
+    return totalWage+dailyWage;
+}
+console.log("UC-7A Using reduce helper function to calculate totalwage: "+ EmpDailyWageArray.reduce(GetTotalWageUsingReduce))
+
+let dayCount= 0;
+function GetDayAlongWithWage(dailyWage)
+{
+    dayCount++
+    return " day" + dayCount + ": " +dailyWage ;
+}
+let dayAlongWithWage = EmpDailyWageArray.map(GetDayAlongWithWage)
+console.log("UC-7B Using map helper function to generate day along with wage: "+ dayAlongWithWage)
+
+function GetFullTimeDays(input)
+{
+    if(input.includes("160"))
+    return input;
+}
+let fullTimeDays = dayAlongWithWage.filter(GetFullTimeDays)
+console.log("UC-7C Using filter helper function finding fulltime wage:\n "+ fullTimeDays)
+console.log("UC-7D Using find helper function finding 1st occurence of fulltime wage:\n "+ fullTimeDays.find(GetFullTimeDays))
+console.log("UC-7E Using find helper function checking every element is fulltime or not:\n "+ fullTimeDays.every(GetFullTimeDays))
+function GetPartTimeDays(input)
+{
+    if(input==80)
+    return input;
+}
+console.log("UC7F using some helper function finding atleast one parttime wage: "+ EmpDailyWageArray.some(GetPartTimeDays))
+
+function GetTotalWorkingDays(totalWorkingDays, dailyWage)
+{
+    if(dailyWage>0)
+    return totalWorkingDays+1;
+    else 
+    return totalWorkingDays;
+}
+
+
+console.log("UC7G using reduce helper function finding total number of working days: "+EmpDailyWageArray.reduce(GetTotalWorkingDays, 0))
